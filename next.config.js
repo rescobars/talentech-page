10320d83c478e76a6d2b50346a09776e71aec365
Robin Escobar <rescobar@buildwithin.com>
2025-04-17 12:37:27 -0600

    addd n8n chat

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        styles: {
+          name: 'styles',
+          test: /\.(css|scss)$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,40 @@
 "use client";
-
+import '@n8n/chat/style.css';
 import { ThemeProvider } from "next-themes";
+import { createChat } from "@n8n/chat";
+import { useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    createChat({
+      webhookUrl: 'http://localhost:5678/webhook/a889d2ae-2159-402f-b326-5f61e90f602e/chat',
+      webhookConfig: {
+        method: 'POST',
+        headers: {}
+      },
+      target: '#n8n-chat',
+      mode: 'window',
+      chatInputKey: 'chatInput',
+      chatSessionKey: 'sessionId',
+      metadata: {},
+      showWelcomeScreen: false,
+      defaultLanguage: 'en',
+      initialMessages: [
+        'Hola! 👋',
+        'Mi nombre es Patricio. ¿Cómo puedo ayudarte hoy?'
+      ],
+      i18n: {
+        en: {
+          title: 'Hola! 👋',
+          subtitle: "Comienza una conversación. Estamos aquí para ayudarte 24/7.",
+          footer: '',
+          getStarted: 'Nueva Conversación',
+          inputPlaceholder: 'Escribe tu pregunta..',
+          closeButtonTooltip: ''
+        },
+      },
+    });
+  }, []);
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
